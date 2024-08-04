4e519e728575d8924d9c8e9c1b759dea2959ce99
kennethdashensheridan <kennethdashensheridan>
2024-08-04 14:48:58 -0700

    added first script

--- k8s_benchmark/target/debug/deps/k8s_benchmark-14bbea3e418f0e3c.d
+++
@@ -1,5 +0,0 @@
-/Users/kennethsheridan/k8s_benchmark/k8s_benchmark/target/debug/deps/libk8s_benchmark-14bbea3e418f0e3c.rmeta: src/main.rs
-
-/Users/kennethsheridan/k8s_benchmark/k8s_benchmark/target/debug/deps/k8s_benchmark-14bbea3e418f0e3c.d: src/main.rs
-
-src/main.rs:
--- k8s_benchmark/target/debug/deps/k8s_benchmark-60aff1812f4c4ebb.d
+++
@@ -1,5 +0,0 @@
-/Users/kennethsheridan/k8s_benchmark/k8s_benchmark/target/debug/deps/libk8s_benchmark-60aff1812f4c4ebb.rmeta: src/main.rs
-
-/Users/kennethsheridan/k8s_benchmark/k8s_benchmark/target/debug/deps/k8s_benchmark-60aff1812f4c4ebb.d: src/main.rs
-
-src/main.rs:
